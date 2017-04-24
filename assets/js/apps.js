77d4ed32835c51e4f9910f0fd65b80646b13323a
lulublondet <maria.mlvp@gmail.com>
2017-04-24 13:47:09 -0500

    Aplicando el centrado y la carga de imagenes al levartar el viewport

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,3 +1,20 @@
+
+window.addEventListener("load", function(){
+
+	var urlImage = ["assets/img/uno.jpg",
+					"assets/img/dos.jpg",
+					"assets/img/tres.jpg",
+					"assets/img/cuatro.jpg",
+					"assets/img/cinco.jpg",
+					"assets/img/seis.jpg"]	
+	
+	for(i in urlImage) {
+		creandoDiv(urlImage[i]);}
+	})
+
+
+
+
 function creandoDiv(getURL) {
  	var  contenedor = document.getElementById("contenedor");
  	var div = document.createElement("div");
@@ -6,22 +23,20 @@ function creandoDiv(getURL) {
  	div.classList.add("contenedor-imagen");
  	div.appendChild(img);
  	contenedor.appendChild(div);
-
-
 }
 
 
 
-
-
 var inputURL = document.getElementById("url");
-inputURL.onkeydown = getURL;
+	inputURL.addEventListener("keydown", getURL);
 
 function getURL(e) {
 	console.log(e);
 	if(e.keyCode==13) {
 		creandoDiv(this.value);
-		this.value = "";
+		this.value = " ";
 	}
 }
+
+
 
